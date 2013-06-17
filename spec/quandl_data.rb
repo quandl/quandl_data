faa5ea288814f55ed32210c7deec941d410994e5
Blake Hilscher <blake@hilscher.ca>
2013-06-17 15:57:24 -0400

    some specs

--- spec/quandl_data.rb
+++ spec/quandl_data.rb
@@ -1,8 +1,28 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe Quandl::Data do
+describe Quandl::Data::Table do
+  subject { Quandl::Data::Random.table( nils: false, rows: 4, columns: 4 ) }
   
-  it { should respond_to :perform }
-
+  its(:to_csv){ should be_a String }
+  its(:to_h){ should be_a Hash }
+  its(:count){ should eq 4 }
+  
+  it "should parse csv" do
+    Quandl::Data::Table.new(subject.to_csv).count.should eq 4
+  end
+  
+  it "should transform the data" do
+    value = subject.first[1]
+    subject.transform(:rdiff).first[1].should_not eq value
+  end
+  
+  it "should collapse the data" do
+    subject.collapse(:monthly).count.should eq 1
+  end
+  
+  it "should convert julian dates to dates" do
+    subject.to_date.first.first.should be_a Date
+  end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 
 require "rspec"
-require "scope_builder"
+require "quandl/data"
