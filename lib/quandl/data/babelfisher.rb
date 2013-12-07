720f91006595f058516c9249dda5e91798f559e1
Blake Hilscher <blake@hilscher.ca>
2013-12-07 17:34:28 -0500

    add cleaned? cleaned! for tracking whether or not a Quandl::Data instance has already been cleaned. #clean will short circuit if the data is already cleaned

--- lib/quandl/data/babelfisher.rb
+++ lib/quandl/data/babelfisher.rb
@@ -13,20 +13,45 @@ module Babelfisher
   def headers?
     headers.present?
   end
+  
+  def cleaned
+    @cleaned
+  end
+  def cleaned=(value)
+    @cleaned = (value == true)
+  end
+  def cleaned?
+    cleaned == true
+  end
 
   protected
   
   def clean(data)
+    t1 = Time.now
+    # skip cleaning if already clean
+    return data if data.kind_of?(Array) && cleaned?
     # Quandl::Data is already clean, but to avoid errors extract internal array
-    return data.data_array if data.is_a?(Quandl::Data)
+    return data.data_array if data.kind_of?(Quandl::Data)
     # Quandl::Babelfish::Data is already clean
-    return data if data.is_a?(Quandl::Babelfish::Data)
+    return data if data.kind_of?(Quandl::Babelfish::Data)
     # Return empty array if given empty string, nil, etc.
     return [] if data.blank?
-    # otherwise parse and clean
-    data = CSV.parse( data ) if data.is_a?(String)
+    # String needs conversion to array
+    data = Format.csv_to_array( data ) if data.kind_of?(String)
+    # Hash needs conversion to array
+    data = Format.hash_to_array( data ) if data.kind_of?(Hash)
+    # Array needs cleaning
     data = Quandl::Babelfish.clean( data )
+    # mark data as clean
+    cleaned!
+    # return data
     data
+  ensure
+    puts "clean #{t1.elapsed_ms} #{data.count}" if t1.elapsed > 0.01
+  end
+
+  def cleaned!
+    self.cleaned = true
   end
   
 end
